a9640337c4056662a57b8dde82347983b871bcf7
Abdulkadir Abdirahman <cccabdalla@gmail.com>
2019-12-30 10:20:57 -0800

    Installing morgan package for logging endpoints

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
 app.use(function(req,res,next){
     res.status(200).json({
         msg: 'it works'
