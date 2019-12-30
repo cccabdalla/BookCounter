b7cbe52b9a661a1e07995f821c9ddc6879dbd809
Abdulkadir Abdirahman <cccabdalla@gmail.com>
2019-12-30 11:18:39 -0800

    Adding routes to handle post incoming requests

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const routes = require('./routes');
+const bodyParser = require('body-parser');
 
 app.use(morgan('dev'));
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/',routes.router);
 
 module.exports = app;
--- routes.js
+++ routes.js
@@ -8,4 +8,12 @@ router.get('/menus/getmenus',function(req,res,next){
     });
 });
 
+router.post('/menus/addmenus',function(req,res,next){
+    console.log(req.body);
+    res.status(200).json({
+        msg: 'Menus added to db',
+        req: JSON.stringify(req.body)
+    });
+});
+
 module.exports.router = router;
