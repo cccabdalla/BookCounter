e65ad11b1a476cd00873d04e0803f2107007556c
cccabdalla <cccabdalla@gmail.com>
2019-12-31 04:57:54 -0800

    Using connection everywhere

--- routes.js
+++ routes.js
@@ -1,24 +1,12 @@
 const express = require('express');
-const dbAssist = require('./oracle/oracleDbAssist');
 const StoredProc = require('./storedProcedures/userStoredProc');
 const router = express.Router();
 
 router.get('/employees/getemployees',function(req,res,next){
-dbAssist.getConnection(function(err,conn){
-    if(err)
-        return console.log('Database connection failed');
-    const sp = new StoredProc().sp_emp_getemployees;
-    dbAssist.executeSqlWithConn(sp,false,conn,function(err,result){
-        if(err)
-            return console.log('Executing sp - ' + sp.name + ' failed - ' + err);
-
-            res.status(200).json({
-                msg: 'Menus received from db',
-                employees: JSON.stringify(result)
-            });
-
+    res.status(200).json({
+        msg: 'Menus received from db',
+        employees: JSON.stringify(result)
     });
-}); 
 });
 
 router.post('/menus/addmenus',function(req,res,next){
--- server.js
+++ server.js
@@ -3,8 +3,20 @@ const app = require('./app');
 
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
+const conn;
+dbAssist.getConnection(function(err,conn){
+    if(err)
+        return console.log('Database connection failed');
 
+        console.log('Database connection initialized');
 
-server.listen(port,function(){
-    console.log('Api is running on localhost:'+port);
+        server.listen(port,function(){
+            console.log('Api is running on localhost:'+port);
+        });
+
+        conn = conn;
 });
+
+module.exports.getConnection = function(){
+    return conn;
+}
