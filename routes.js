143ffcf6f88dac8e5b93f5f60ea8721eb1f8c1e9
cccabdalla <cccabdalla@gmail.com>
2019-12-31 01:38:55 -0800

    Connecting api to Oracle Database

--- routes.js
+++ routes.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const dbAssist = require('./oracle/oracleDbAssist');
+const StoredProc = require('./storedProcedures/userStoredProc');
 const router = express.Router();
 
+router.get('/employees/getemployees',function(req,res,next){
+dbAssist.getConnection(function(err,conn){
+    if(err)
+        return console.log('Database connection failed');
+    const sp = new StoredProc().sp_emp_getemployees;
+    dbAssist.executeSqlWithConn(sp,false,conn,function(err,result){
+        if(err)
+            return console.log('Executing sp - ' + sp.name + ' failed - ' + err);
 
-router.get('/menus/getmenus',function(req,res,next){
-    res.status(200).json({
-        msg: 'Menus received from db'
+            res.status(200).json({
+                msg: 'Menus received from db',
+                employees: JSON.stringify(result)
+            });
+
     });
+}); 
 });
 
 router.post('/menus/addmenus',function(req,res,next){
